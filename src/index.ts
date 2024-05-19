c2a9cbdf7211fe4d177d7fdc4a89324eee308dec
bart zabielski <idointernetbz@outlook.com>
2024-05-19 15:08:37 +1000

    type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,52 @@
 // functions in typescript
+// function overloading
+// https://www.udemy.com/course/typescript-course/learn/lecture/31786238#overview
 console.log("start");
 
-// function declaration
-function intro(name: string, age: number, country?: string): string {
-  if (country) {
-    return `my name is ${name} and age is ${age} and I live in ${country}`;
-  }
-  return `my name is ${name} and age is ${age}`;
-}
+type Reservation = {
+  departureDate: Date;
+  returnDate?: Date;
+  departingFrom: string;
+  destination: string;
+};
 
-console.log(intro("john", 32));
+// using overloading with different params.
+// ticket with return trip & tickt one way single trip
+
+type Reserve = {
+  (
+    departureDate: Date,
+    returnDate: Date,
+    departingFrom: string,
+    destination: string
+  ): Reservation | never;
+  (departureDate: Date, departingFrom: string, destination: string):
+    | Reservation
+    | never;
+};
+
+const reserve: Reserve = (
+  departureDate: Date,
+  returnDateOrDepartingFrom: Date | string,
+  departingFromOrDestination: string,
+  destination?: string
+) => {
+  if (returnDateOrDepartingFrom instanceof Date && destination) {
+    return {
+      departureDate: departureDate,
+      returnDate: returnDateOrDepartingFrom,
+      departingFrom: departingFromOrDestination,
+      destination: destination,
+    };
+  } else if (typeof returnDateOrDepartingFrom === "string") {
+    return {
+      departureDate: departureDate,
+      departingFrom: returnDateOrDepartingFrom,
+      destination: departingFromOrDestination,
+    };
+  }
+  throw new Error("Please provide valid details to reserve a ticket");
+};
+
+console.log(reserve(new Date(), new Date(), "New York", "Washington"));
+console.log(reserve(new Date(), "New York", "Washington"));
--- src/sec-3-functions-in-typescript/functions.ts
+++ src/sec-3-functions-in-typescript/functions.ts
@@ -1,19 +1,75 @@
+// // functions in typescript
+// console.log("start");
+
+// // function declaration
+// function intro(name: string, age: number): string {
+//   return `my name is ${name} and age is ${age}`;
+// }
+
+// // function expression
+// const intro2 = function intro(name: string, age: number): string {
+//   return `My name is ${name} and age is ${age}`;
+// };
+
+// // arrow function
+// const intro3 = (name: string, age: number) => {
+//   return `my name is ${name} and age is ${age}`;
+// };
+
+// export default {};
+
+//
+
 // functions in typescript
+// includes function call signatures
+// https://www.udemy.com/course/typescript-course/learn/lecture/31786232#overview
+
 console.log("start");
 
 // function declaration
-function intro(name: string, age: number): string {
+function intro(name: string, age: number, country?: string): string {
+  if (country) {
+    return `my name is ${name} and age is ${age} and I live in ${country}`;
+  }
   return `my name is ${name} and age is ${age}`;
 }
 
-// function expression
-const intro2 = function intro(name: string, age: number): string {
-  return `My name is ${name} and age is ${age}`;
+console.log(intro("john", 32));
+console.log("john", 32, "canada");
+
+enum AgeUnit {
+  years = "years",
+  months = "months",
+}
+
+type greetingFunction = (greeting: string) => string;
+
+type Person = {
+  name: string;
+  age: number;
+  ageUnit: AgeUnit;
+  country: string;
+  // greet: (greeting: string, ageUnit: AgeUnit) => {};
+  greet: greetingFunction;
 };
 
-// arrow function
-const intro3 = (name: string, age: number) => {
-  return `my name is ${name} and age is ${age}`;
+const person: Person = {
+  name: "scott",
+  age: 30.5,
+  ageUnit: AgeUnit.years,
+  country: "USA",
+  greet: (greeting) => {
+    return `${greeting} ${person.name}`;
+  },
 };
+
+function convertAgeToMonths(person: Person): Person {
+  person.age = person.age * 12;
+  person.ageUnit = AgeUnit.months;
+  return person;
+}
+
+// console.log(convertAgeToMonths(person));
+console.log(person.greet(" Good Morning"));
 
 export default {};
